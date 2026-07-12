61e0d3a9422dd25af85114e982dcf12690b3d417
pv16061995 <pv16061995@gmail.com>
2026-07-12 10:50:18 +0530

    upload admin panel file

---
+++ adminpanel/application/controllers/Dashboard.php
@@ -0,0 +1,21 @@
+
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public function index()
+	{
+        $data = array();
+		$data['title'] = 'Dashboard';
+        $data['page']  = 'dashboard/index';
+
+        $this->load->view('theme/layout', $data);
+	}
+
+	public function home()
+	{
+		echo "Welcome to the home page!";
+	}
+}
---
+++ adminpanel/application/views/layout/master.php
@@ -0,0 +1,11 @@
+<?php // $this->load->view('layout/header'); ?>
+
+<?php // $this->load->view('layout/topbar'); ?>
+
+<?php // $this->load->view('layout/sidebar'); ?>
+
+<?php $this->load->view($page); ?>
+
+<?php // $this->load->view('layout/footer'); ?>
+
+<?php // $this->load->view('layout/scripts'); ?>
